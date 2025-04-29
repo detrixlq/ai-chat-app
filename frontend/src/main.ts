d48df7ef218c3491d0f14c78a05bdde97ecdeca6
detrixlq <detrixlq@gmail.com>
2025-04-30 01:17:12 +0500

    dockerized

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -3,7 +3,6 @@ import { appConfig } from './app/app.config';
 import { AppComponent } from './app/app.component';
 import { provideRouter } from '@angular/router';
 import { routes } from './app/app.routes';
-
 import { importProvidersFrom } from '@angular/core';
 import { HttpClientModule, HttpClient } from '@angular/common/http';
 import { MarkdownModule } from 'ngx-markdown';
